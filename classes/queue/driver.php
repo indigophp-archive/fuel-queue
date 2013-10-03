220366c7e0d9fb390f51acb7898b0e5c2b8d9c06
Sági-Kazár Márk <webmaster@firstcomputer.hu>
2013-10-03 18:00:36 +0200

    Config getter setter

--- classes/queue/driver.php
+++ classes/queue/driver.php
@@ -30,27 +30,34 @@ abstract class Queue_Driver
 	}
 
 	/**
-	* Get a driver config setting.
+	* Get a driver config setting
 	*
-	* @param string $key the config key
-	* @param mixed  $default the default value
-	* @return mixed the config setting value
+	* @param	string|null		$key		Config key
+	* @param	mixed			$default	Default value
+	* @return	mixed						Config setting value or the whole config array
 	*/
-	public function get_config($key, $default = null)
+	public function get_config($key = null, $default = null)
 	{
-		return \Arr::get($this->config, $key, $default);
+		return is_null($key) ? $this->config : \Arr::get($this->config, $key, $default);
 	}
 
 	/**
-	* Set a driver config setting.
+	* Set a driver config setting
 	*
-	* @param string $key the config key
-	* @param mixed $value the new config value
-	* @return object $this for chaining
+	* @param	string|array	$key		Config key or array of key-value pairs
+	* @param	mixed			$value		New config value
+	* @return	$this						$this for chaining
 	*/
-	public function set_config($key, $value)
+	public function set_config($key, $value = null)
 	{
-		\Arr::set($this->config, $key, $value);
+		if (is_array($key))
+		{
+			$this->config = \Arr::merge($this->config, $key);
+		}
+		else
+		{
+			\Arr::set($this->config, $key, $value);
+		}
 
 		return $this;
 	}
