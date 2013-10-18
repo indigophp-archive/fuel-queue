4411a06de420ac3390f7a4d1f475d6dffd4d98c9
Sági-Kazár Márk <webmaster@firstcomputer.hu>
2013-10-18 14:29:13 +0200

    Work option added, shutdown callback unregister on work

--- tasks/queue.php
+++ tasks/queue.php
@@ -6,7 +6,19 @@ use \Phresque\Worker;
 
 class Queue
 {
+	/**
+	 * Logger instance
+	 *
+	 * @var \Psr\Log\LoggerInterface
+	 */
 	protected $logger;
+
+	/**
+	 * Shutdown callable
+	 *
+	 * @var callable
+	 */
+	protected $shutdown;
 
 	public function __construct()
 	{
@@ -37,10 +49,12 @@ class Queue
 
 		$this->logger = $logger;
 
-		// Register shutdown function to catch exit
-		\Event::register('shutdown', function() use($logger) {
+		$this->shutdown = function() use($logger) {
 			$logger->warning('Worker {pid} is stopping', array('pid' => getmypid()));
-		});
+		};
+
+		// Register shutdown function to catch exit
+		\Event::register('shutdown', $this->shutdown);
 	}
 
 	public function run($connection = 'default')
@@ -55,6 +69,18 @@ class Queue
 
 		$worker = new Worker($config['queue'], $config['driver'], $config['connection']);
 		$worker->setLogger($this->logger);
-		$worker->listen();
+		$work = \Cli::option('work', \Cli::option('w', false));
+
+		if ($work)
+		{
+			$worker->work();
+
+			// Unregister shutdown function to catch exit
+			\Event::unregister('shutdown', $this->shutdown);
+		}
+		else
+		{
+			$worker->listen();
+		}
 	}
 }
