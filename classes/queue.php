3a924f54d33b1a354e76d5f0b7f205688dbca1c4
Sági-Kazár Márk <mark.sagikazar@gmail.com>
2014-01-29 12:59:59 +0100

    Added copyright

--- classes/queue.php
+++ classes/queue.php
@@ -7,6 +7,7 @@
  * @version     2.0
  * @author      Márk Sági-Kazár <mark.sagikazar@gmail.com>
  * @license     MIT License
+ * @copyright   2013 - 2014 Indigo Development Team
  * @link        https://indigophp.com
  */
 
