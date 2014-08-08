2c87fe01fe1c581e6dc1c31c7de342445d5bf3d7
Sági-Kazár Márk <mark.sagikazar@gmail.com>
2014-08-08 23:36:26 +0200

    Adds file header

--- tasks/worker.php
+++ tasks/worker.php
@@ -1,9 +1,23 @@
 <?php
 
+/*
+ * This file is part of the Fuel Queue package.
+ *
+ * (c) Indigo Development Team
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 declare(ticks = 1);
 
 namespace Fuel\Tasks;
 
+/**
+ * Worker Task
+ *
+ * @author Márk Sági-Kazár <mark.sagikazar@gmail.com>
+ */
 class Worker
 {
 	public function __construct()
