220a1da13f36374de14edd59f21dec5e2ca8c892
Sági-Kazár Márk <mark.sagikazar@gmail.com>
2014-08-13 13:47:46 +0200

    Adds Codeception Fuel module

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -1,13 +1,4 @@
 <?php
 // This is global bootstrap for autoloading
 
-$_SERVER['doc_root']     = __DIR__ . '/../../../../';
-$_SERVER['app_path']     = __DIR__ . '/../../../app/';
-$_SERVER['core_path']    = __DIR__ . '/../../../core/';
-$_SERVER['package_path'] = __DIR__ . '/../../';
-$_SERVER['vendor_path']  = __DIR__ . '/../../../vendor/';
-
-require_once __DIR__ . '/../../../core/bootstrap_phpunit.php';
-require_once __DIR__ . '/../vendor/autoload.php';
-
-\Package::load('queue');
+require __DIR__.'/../vendor/autoload.php';
--- tests/unit/_bootstrap.php
+++ tests/unit/_bootstrap.php
@@ -1,2 +1,6 @@
 <?php
 // Here you can initialize variables that will be available to your tests
+
+$package = \Codeception\Configuration::projectDir();
+
+\Package::load('queue', $package);
