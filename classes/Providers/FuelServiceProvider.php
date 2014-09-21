5b9ddd392aa596adcc8e22008dfccb8a07bb4ee2
Sági-Kazár Márk <mark.sagikazar@gmail.com>
2014-09-21 03:14:43 +0200

    Adds initial beanstalk support

--- classes/Providers/FuelServiceProvider.php
+++ classes/Providers/FuelServiceProvider.php
@@ -12,6 +12,8 @@
 namespace Indigo\Fuel\Queue\Providers;
 
 use Fuel\Dependency\ServiceProvider;
+use Indigo\Queue\Connector;
+use Indigo\Queue\Queue;
 
 /**
  * Provides Queue service
@@ -26,10 +28,10 @@ class FuelServiceProvider extends ServiceProvider
 	public $provides = [
 		'queue',
 		'worker',
-		'queue.connector.beanstalk',
-		'queue.connector.direct',
-		'queue.connector.iron',
-		'queue.connector.rabbit',
+		'queue.beanstalk',
+		'queue.direct',
+		'queue.iron',
+		'queue.rabbit',
 	];
 
 	/**
@@ -42,6 +44,15 @@ class FuelServiceProvider extends ServiceProvider
 	public function __construct()
 	{
 		\Config::load('queue', true);
+
+		$config = \Config::get('queue', []);
+		$this->defaultConfig = \Arr::filter_keys($config, ['queues'], true);
+
+		// We don't have defined queues
+		if ($queues = \Arr::get($config, 'queues', false) and ! empty($queues))
+		{
+			\Config::set('queue.queues.__default__', []);
+		}
 	}
 
 	/**
@@ -49,9 +60,42 @@ class FuelServiceProvider extends ServiceProvider
 	 */
 	public function provide()
 	{
-		$this->register('queue', function($dic, $name, $connector)
+		$this->register('queue', function($dic, $name, $config = [])
 		{
+			if (is_array($config))
+			{
+				$connector = \Arr::get($config, 'connector', '');
+				\Arr::delete($config, 'connector');
+			}
 
+			$config = array_merge($this->defaultConfig, \Config::get('queue.queues.', $name, []), $config);
+
+			// determine the connector to load
+			if ($config['connector'] instanceof Connector)
+			{
+				$connector = $config['connector'];
+			}
+			elseif (strpos('\\', $config['connector']) !== false and class_exists($config['connector']))
+			{
+				$connector = $config['connector'];
+			}
+			else
+			{
+				$connector = 'queue.'.strtolower($config['connector']);
+			}
+
+			// try to resolve the connector if necessarry
+			if ( ! $config['connector'] instanceof Connector)
+			{
+				$connector = $dic->multiton($connector, $name, $config);
+			}
+		});
+
+		$this->register('queue.beanstalk', function($dic, $name, array $config = [])
+		{
+			$pheanstalk = $dic->resolve('Pheanstalk\\Pheanstalk', \Arr::filter_keys($config, ['host', 'port']));
+
+			return $dic->resolve('Indigo\\Queue\\Connector\\BeanstalkConnector', [$pheanstalk]);
 		});
 	}
 }
